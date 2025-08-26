f83d7a5e7274ff3c76aa8e69723bb38a88ece047
sitfuturz <sarimitfuturz@gmail.com>
2025-08-26 12:57:04 +0530

    adding

--- src/env/env.local.ts
+++ src/env/env.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // baseURL: 'http://localhost:2027',
-  baseURL: 'https://gr72tgrc-2027.inc1.devtunnels.ms',
+   baseURL: 'http://localhost:2027',
+ 
   route: 'admin',
   imageUrl:'https://commutee.itfuturz.in/'
   };
